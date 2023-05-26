7df09c050b1c1c63226c0ead026c1e4faab4e8c7
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-05-26 13:23:48 +0200

    Implement authorisation for task creation page

--- pages/projects/[id]/tasks/create.tsx
+++ pages/projects/[id]/tasks/create.tsx
@@ -1,18 +1,56 @@
 import { useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import TaskForm from "../../../../components/TaskForm";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { PrismaClient } from "@prisma/client";
+import { useRouter } from "next/router";
+import { Alert } from "@mui/material";
 
-export default function Create() {
+export default function Create({ isAuthorised }: { isAuthorised: boolean }) {
     const { user, isLoading } = useUser();
+
+    const router = useRouter();
 
     useEffect(() => {
         if (!isLoading && !user) window.location.href = '/api/auth/login';
     }, [user, isLoading]);
 
+    useEffect(() => {
+        if (!isAuthorised) {
+            setTimeout(() => {
+                router.push('/');
+            }, 3000);
+        }
+    }, [isAuthorised])
+
     return (
-        <TaskForm
-            user={user}
-            method="POST"
-        />
+        <>
+            {!isAuthorised ? <Alert severity="error">You are not authorised to access this page. You will be redirected to the homepage.</Alert> : <TaskForm
+                user={user}
+                method="POST"
+            />}
+        </>
     )
 }
+
+export const getServerSideProps = withPageAuthRequired({
+    async getServerSideProps(ctx) {
+        const session = await getSession(ctx.req, ctx.res);
+        const prisma = new PrismaClient();
+        const { id } = ctx.query;
+
+        const project = await prisma.project.findUnique({
+            where: {
+                id: id as string
+            }
+        })
+
+        const isAuthorised = project?.creator === session?.user.email;
+
+        return {
+            props: {
+                isAuthorised
+            }
+        }
+    },
+});
