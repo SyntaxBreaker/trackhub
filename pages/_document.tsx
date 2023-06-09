3f81a56fc1566b1fdcce75ead7e2b6d298649055
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-06-09 14:11:48 +0200

    Improve SEO

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta
+                    name="description"
+                    content="Effortlessly manage projects with our powerful 
+                        project management app. Stay organized, 
+                        collaborate with your team, and track progress all
+                        in one place. Increase productivity, communication,
+                        and never miss a deadline again. Try our project 
+                        management app today and experience project 
+                        execution from start to finish."
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
