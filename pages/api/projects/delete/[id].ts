97eefdf0bb67895a7b86c16266991d41306a3e20
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-12-20 12:57:20 +0100

    Fix typos

--- pages/api/projects/delete/[id].ts
+++ pages/api/projects/delete/[id].ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handle(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
